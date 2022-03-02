85b2ad80e9d14a9642e70e951afd6fcf3e55e8a3
Pedro Brandão <phbrandao@outlook.com>
2022-03-01 21:38:35 -0300

    Configured routes

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+
+import { BsGithub } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+
+function Navbar() {
+   
+  return (
+    <div>
+      <nav className="bg-gray-800 shadow" role="navigation">
+        <div className="container mx-auto p-4 flex flex-wrap items-center md:flex-no-wrap">
+          <div className="mr-4 md:mr-8 centered-label">
+            <a href="https://github.com">
+              <BsGithub
+                className="text-gray-300 hover:text-gray-400 mr-4"
+                size={40}
+              />
+            </a>
+            <p className="text-xl font-extralight uppercase text-gray-300">
+              Buscador de repositórios
+            </p>
+          </div>
+
+          <div className="w-full md:w-auto md:flex-grow md:flex md:items-center">
+            <ul className="flex flex-col block mt-4 -mx-4 pt-4 md:flex-row md:items-center md:mx-0 md:ml-auto md:mt-0 md:pt-0">
+              <li>
+              <Link
+                  className="text-gray-300 hover:text-gray-400 font-medium mr-4" 
+                  to="/"
+                  >
+                  BUSCA
+                </Link>
+                <Link
+                  className="text-gray-300 hover:text-gray-400 font-medium mr-4" 
+                  to="/about"
+                  >
+                  SOBRE
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './about';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route exact path="/" element={<App/>} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
