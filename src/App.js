194fe7f5ae03f8dc471ff3deab02cd9e905814b7
Pedro Brandão <phbrandao@outlook.com>
2022-03-01 21:33:07 -0300

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  
   return (
     <div className="App">
-      Teste
+      Navbar
+
+      <main className="App">
+        <div className="text-gray-900 text-xl mb-12 mt-12 justify-center content-center">
+          Encontre repositórios públicos de organizações do {" "}
+          <a className="text-blue-700" href="https://github.com">
+            GitHub
+          </a>
+        </div>
+
+        <div>
+          SearchBar
+        </div>
+      </main>
     </div>
   );
 }
